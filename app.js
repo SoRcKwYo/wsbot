add7b460842a6f835796bcf30ac59c3ac8eb976a
SoRcKwYo <hp04m1c14@gmail.com>
2025-04-30 16:56:00 +0800

    feat: 移除不必要的用戶代理設置，簡化配置邏輯

--- app.js
+++ app.js
@@ -194,11 +194,7 @@ class WhatsAppBot {
         type: "remote",
         remotePath:
           "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1020491273-alpha.html",
-      },
-      userAgent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36", // 使用與您環境匹配的用戶代理
-      fallbackUserAgent:
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+      }
     });
 
     const functionDir = path.join(__dirname, "data", "functions");
